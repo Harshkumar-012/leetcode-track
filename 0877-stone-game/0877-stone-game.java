c3c11691ce32f0f5344c2385337585047847cf7a
Harshkumar-012 <hk6951259@gmail.com>
2026-08-02 17:26:38 +0530

    Time: 5 ms (23.31%), Space: 43.6 MB (22.79%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,17 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        Arrays.sort(piles);
+        int n = piles.length;
+        int st = 0;
+        int ed = n-1;
+        int alice = 0;
+        int bob = 0;
+        while(st<ed){
+            alice+=piles[ed];
+            bob+=piles[st];
+            st++;
+            ed--;
+        }
+        return alice>bob;
     }
 }
