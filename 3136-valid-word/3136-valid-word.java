0a7ae8bcc156977421e4ca2913cb23a5138823cb
Harshkumar-012 <hk6951259@gmail.com>
2026-08-27 09:35:43 +0530

    Time: 1 ms (99.74%), Space: 43.1 MB (93.12%) - LeetHub

---
+++ 3136-valid-word/3136-valid-word.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValid(String word) {
+        int n = word.length();
+        if(n<3){
+            return false;
+        }
+        boolean isVowel = false;
+        boolean isConstant = false;
+        for(int i=0;i<n;i++){
+            char ch = word.charAt(i);
+            if(!Character.isLetterOrDigit(ch)){
+                return false;
+            }
+            if(Character.isLetter(ch)){
+                if(ch=='a' || ch=='e' || ch=='i' || ch=='o'|| ch=='u' ||
+                ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U'){
+                    isVowel = true;
+                }
+                else{
+                    isConstant = true;
+                }
+            }
+        }
+        return isVowel && isConstant;
+    }
+}
