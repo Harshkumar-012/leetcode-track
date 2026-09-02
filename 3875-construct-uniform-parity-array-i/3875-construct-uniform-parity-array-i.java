b3efc240bb69182a67d9995dbcf6f18c011e31d1
Harshkumar-012 <hk6951259@gmail.com>
2026-09-02 14:18:08 +0530

    Time: 0 ms (100%), Space: 45.3 MB (16.73%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,18 +1,5 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-        int n = nums1.length;
-        // int[] check = new int[n];
-        // for(int i=0;i<n;i++){
-        //     check[i] = nums1[i];
-        // }
-         int count = 0;
-         int count1 = 0;
-        for(int i=0;i<n;i++){
-            if(nums1[i]%2==0){
-                count++;
-            }
-            count1++;
-        }
-        return count==n || count1==n;
+        return true;
     }
 }
