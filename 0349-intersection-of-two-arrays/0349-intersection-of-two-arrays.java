72479dd4e362ccad881114a6e189af5e6c2cd532
Harshkumar-012 <hk6951259@gmail.com>
2026-09-04 23:59:49 +0530

    Time: 3 ms (47.25%), Space: 45.2 MB (25.12%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -5,20 +5,36 @@ class Solution {
         for(int val : nums1){
             seen.add(val);
         }
-        int i=0;
-        
         for(int val : nums2){
-            if(seen.contains(val)){
-                seen1.add(val);
-                
-            }
+            seen1.add(val);
         }
-        int[] ans = new int[seen1.size()];
+        seen.retainAll(seen1);
+        int[] ans = new int[seen.size()];
         int count=0;
-        for(int val : seen1){
+        for(int val : seen){
             ans[count] = val;
             count++;
         }
         return ans;
+        // HashSet<Integer>seen = new HashSet<>();
+        // HashSet<Integer>seen1 = new HashSet<>();
+        // for(int val : nums1){
+        //     seen.add(val);
+        // }
+        // int i=0;
+        
+        // for(int val : nums2){
+        //     if(seen.contains(val)){
+        //         seen1.add(val);
+                
+        //     }
+        // }
+        // int[] ans = new int[seen1.size()];
+        // int count=0;
+        // for(int val : seen1){
+        //     ans[count] = val;
+        //     count++;
+        // }
+        // return ans;
     }
 }
